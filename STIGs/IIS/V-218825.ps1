acda2150a14ad947092c39ee3382633dbe8d0bab
jlongo62 <joseph.longo@live.com>
2022-07-20 09:03:24 -0400

    Update V-218825.ps1

--- STIGs/IIS/V-218825.ps1
+++ STIGs/IIS/V-218825.ps1
@@ -1,17 +1,25 @@
-﻿#########################################################
-# Source: https://github.com/Average-Bear/Configure-StigIIS/blob/master/Configure-StigIIS.ps1
-###################################################################################
 
 <#
+
+**************************************************************************
+* Source: https://github.com/Average-Bear/Configure-StigIIS/blob/master/Configure-StigIIS.ps1
+**************************************************************************
+
 .SYNOPSIS 
     Configure and verify Authorization Rules settings for vulnerability 76771.
 .DESCRIPTION
     Configure and verify Authorization Rules settings for vulnerability 76771.
 .Impacted Files:
     C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Config\web.config
+
+
+audit file entry:
+     value_data  : '<allow .*groups="(\\\\.*\\\\)?[Aa]dministrators".*\\/>'
+     appcmd_args : "list config /section:system.web/authorization"
+See changes as tenable with command:
+  c:\windows\system32\inetsrv/appcmd.exe list config /section:system.web/authorization
+      
 #>
-
-
 
 [String]$FilterPath = 'system.web/authorization/allow'
    
@@ -20,8 +28,11 @@ $PreConfigUsers = Get-WebConfigurationProperty -Filter $FilterPath -Name Users
 
 Remove-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT' -Filter "system.web/authorization" -Name "."
 
-#groups is unsupported, but tenable is testing for that in audit v2r5
+# groups is unsupported, but tenable is testing for that in audit v2r5
+# ignored; I thhink the audit file is wrong 
 Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT' -Filter "system.web/authorization" -Name "." -Value @{groups='Administrators'} -Type allow
+
+# works
 Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT' -Filter "system.web/authorization" -Name "." -Value @{roles='Administrators'} -Type allow
 Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT' -Filter "system.web/authorization" -Name "." -Value @{users='?'} -Type deny
 
