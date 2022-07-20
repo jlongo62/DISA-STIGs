f7ee1432aaf03a8e6054a9d9aa73cf8649b3eef1
jlongo62 <joseph.longo@live.com>
2022-07-20 14:06:38 -0400

    Update V-218798.ps1

--- STIGs/IIS/V-218798.ps1
+++ STIGs/IIS/V-218798.ps1
@@ -13,26 +13,6 @@ Set-WebConfigurationProperty //staticContent -Name Collection -InputObject $NewC
 
 $PostMimeConfig = (Get-WebConfiguration //staticcontent).Collection
 
-[PSCustomObject] @{
-           
-    Vulnerability = 'V-76711, V-76797'
-    Computername = $env:COMPUTERNAME
-    PreConfigExtenstions = $PreMimeConfig.FileExtension
-    PreConfigCount = $PreMimeConfig.Count
-    PostConfigurationExtenstions = $PostMimeConfig.FileExtension
-    PostConfigurationCount = $PostMimeConfig.Count
-}
-
-[PSCustomObject] @{
-           
-    Vulnerability = 'V-76711, V-76797'
-    Computername = $env:COMPUTERNAME
-    PreConfigExtenstions = $maps.FileExtension
-    PreConfigCount = $maps.Count
-    PostConfigurationExtenstions = $PostMimeConfig.FileExtension
-    PostConfigurationCount = $PostMimeConfig.Count
-}
-
 #STIG CHECK
 $output=(Get-WebConfiguration -Filter system.webServer/staticContent/mimeMap | Where-object {$_.fileExtension -eq '.dll' } | Select-object fileExtension | Format-Table -HideTableHeaders); foreach-object { if(!$output){ $result='No entries found' } else { $result=$output }; $result };
 $output
