a361f8d4d64700adac58c22a01b5d5db38e10a09
jlongo62 <joseph.longo@live.com>
2023-09-01 12:00:03 -0400

    Update V-213973.sql

--- STIGs/SQL Server/V-213973.sql
+++ STIGs/SQL Server/V-213973.sql
@@ -9,10 +9,10 @@
 USE master;
 GO
 DECLARE @root AS VARCHAR(255);
-SET @root = '\\......\share\SQLServers-ServiceKeyBackup_V-213973\'
+SET @root = '\\management\share\SQL Scripts\SQLServers-ServiceKeyBackup_V-213973\'
 
 DECLARE @password AS VARCHAR(30);
-SET  @password = '.......';
+SET  @password = '....';
 
 DECLARE @path AS VARCHAR(255);
 SET @path =  @root +  REPLACE(@@SERVERNAME,'\','_') + '.key';
@@ -22,14 +22,24 @@ SET @sql = 'BACKUP SERVICE MASTER KEY TO FILE = ''' + @path +  ''' ENCRYPTION BY
 PRINT  @sql 
 EXEC (@sql);
 
+SET @path =  'L:\SQLServers-ServiceKeyBackup_V-213973\' +  REPLACE(@@SERVERNAME,'\','_') + '.key';
+SET @sql = 'BACKUP SERVICE MASTER KEY TO FILE = ''' + @path +  ''' ENCRYPTION BY PASSWORD = ''' +  @password + '''';
+PRINT  @sql 
+EXEC (@sql);
+
 DECLARE @cmd AS VARCHAR(255)
-SET @cmd = 'ECHO ''' +  @password +  ''' >> "' + @root + 'password.txt"';
+SET @cmd = 'ECHO ''' +  @password +  ''' >> "' + @root +  REPLACE(@@SERVERNAME,'\','_') + 'password.txt"';
 PRINT @cmd
+
+DECLARE @cmd2 AS VARCHAR(255)
+SET @cmd2 = 'ECHO ''' +  @password +  ''' >> "' + 'L:\SQLServers-ServiceKeyBackup_V-213973\' +  REPLACE(@@SERVERNAME,'\','_') + 'password.txt"';
+PRINT @cmd2
 /************************************
 * WRITE PAssword file
 ************************************/
 BEGIN TRY
 	exec xp_cmdshell @cmd ;
+	exec xp_cmdshell @cmd2 ;
 END TRY
 
 BEGIN CATCH
@@ -38,9 +48,9 @@ BEGIN CATCH
 	RECONFIGURE WITH OVERRIDE;
 
 	exec xp_cmdshell @cmd ;
+	exec xp_cmdshell @cmd2 ;
 
 	EXECUTE sp_configure 'xp_cmdshell', 0;
 	RECONFIGURE WITH OVERRIDE;
 
 END CATCH
-
